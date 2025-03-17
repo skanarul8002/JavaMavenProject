6c65ce6266618ab93cd8f82fdb7b773ffc42eb35
skanarul8002 <skanarul8002@gmail.com>
2025-03-17 15:41:57 +0530

    fc

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void testMain() {
+        assertEquals(5, 2 + 3, "Sum should be 5");
+    }
+}
